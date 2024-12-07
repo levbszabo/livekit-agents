94ae492c8022970199dd8f54957802450bb4bbee
levbszabo <levbszabo@gmail.com>
2024-12-07 17:47:59 -0500

    update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const withNextPluginPreval = createNextPluginPreval();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  basePath: '/playground',
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/playground/:path*',
+          destination: '/:path*',
+        },
+      ],
+    };
+  },
 };
 
 module.exports = withNextPluginPreval(nextConfig);
